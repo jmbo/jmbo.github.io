b62c1e8539eab5abcff929c33372c6db7047dcd4
jmbo <jmbo@users.noreply.github.com>
2025-07-08 10:06:13 -0400

    making some changes to the site -- adding JS and some animations

---
+++ resources/js/script.js
@@ -0,0 +1,54 @@
+console.log("Script loaded successfully!");
+
+// update the copyright year dynamically
+const cpYear = document.querySelector(".copyright-year");
+cpYear.textContent = new Date().getFullYear();
+
+// typewriter effect for the "who am I" text
+const whoAmI = document.querySelector(".who-am-i");
+const items = [
+  "a developer",
+  "a designer",
+  "an engineer",
+  "an adventurer",
+  "Jose!",
+];
+
+whoAmI.textContent = "";
+function typeWriterEffect() {
+  function backspace() {
+    if (whoAmI.textContent) {
+      whoAmI.textContent = whoAmI.textContent.slice(0, -1);
+      setTimeout(backspace, TYPING_SPEED);
+    } else {
+      setTimeout(type, TYPING_SPEED * 2);
+    }
+  }
+
+  function type() {
+    if (charIndex < currentItem.length) {
+      whoAmI.textContent += currentItem[charIndex];
+      charIndex++;
+      setTimeout(type, TYPING_SPEED);
+    } else if (currentIndex < items.length - 1) {
+      currentIndex++;
+      currentItem = items[currentIndex];
+      charIndex = 0;
+      setTimeout(backspace, WAIT);
+    } else {
+      whoAmI.addEventListener("click", typeWriterEffect);
+    }
+  }
+
+  const TYPING_SPEED = 80;
+  const WAIT = 1000;
+  let currentIndex = 0;
+  let currentItem = items[currentIndex];
+  let charIndex = 0;
+
+  whoAmI.removeEventListener("click", typeWriterEffect);
+  backspace();
+}
+
+// start the typewriter effect
+typeWriterEffect();
